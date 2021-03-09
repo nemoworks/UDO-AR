ffb9d4bcde7e2fee33a52875576446e8fc71cb9a
崔子寒 <cui@cuizihandeMacBook-Air.local>
2021-03-09 16:29:28 +0800

    init

---
+++ ARRemoteController/ARRemoteController/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ARRemoteController
+//
+//  Created by 崔子寒 on 2021/3/8.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# UDO-AR
+# CoreML + ARKit识别空气净化器并加入AR交互
+
+## 进度
+
+- 3月8日：收集了数据集用于训练空气净化器识别模型，使用CreateML框架进行迁移学习，训练效果较好。
+
+  <img src="./doc/result.png" alt="fig" style="zoom:50%;" />
