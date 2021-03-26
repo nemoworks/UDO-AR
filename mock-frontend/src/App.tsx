938c791509f0411fa38bcfee3ebacec58bca21ba
Cuizihan <mg20330012@smail.nju.edu.cn>
2021-03-26 13:58:19 +0800

    update front-end

--- mock-frontend/src/App.tsx
+++ mock-frontend/src/App.tsx
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import { Line } from '@ant-design/charts';
+
+const Page: React.FC = () => {
+  const data = [
+    { year: '1991', value1: 3 },
+    { year: '1992', value: 4 },
+    { year: '1993', value: 3.5 },
+    { year: '1994', value: 5 },
+    { year: '1995', value: 4.9 },
+    { year: '1996', value: 6 },
+    { year: '1997', value: 7 },
+    { year: '1998', value: 9 },
+    { year: '1999', value: 13 },
+  ];
+
+  const config = {
+    data,
+    height: 400,
+    xField: 'year',
+    yField: 'value',
+    point: {
+      size: 5,
+      shape: 'diamond',
+    },
+    label: {
+      style: {
+        fill: '#aaa',
+      },
+    },
+  };
+  return <Line {...config} />;
+};
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Page/>
+      <Page/>
     </div>
   );
 }
+
+
 
 export default App;
